6d058722dba8a26896b9d1bf23c239330b9d64da
devonhackley <devonhackley@gmail.com>
2017-03-03 15:39:46 -0800

    added some more modules to package.json

--- server/test/dummy-test.js
+++ server/test/dummy-test.js
@@ -3,7 +3,7 @@
 const {expect} = require('chai');
 
 describe('Testing the server and Travis', function(){
-  it('should pass the test' () => {
+  it('should pass the test', () => {
     expect(true).to.equal(true);
   });
 });
