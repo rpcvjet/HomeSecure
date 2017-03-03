55454ce578b2e0820bd343ac1cc9b6011680dde9
devonhackley <devonhackley@gmail.com>
2017-03-03 14:47:14 -0800

    setting up travis ci

---
+++ server/test/dummy-test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const {expect} = require('chai');
+
+describe('Testing the server and Travis', function(){
+  it('should pass the test' () => {
+    expect(true).to.equal(true);
+  });
+});
