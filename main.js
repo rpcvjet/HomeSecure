aed6b473a115bb8b174af730d09a2993635e7742
devonhackley <devonhackley@gmail.com>
2017-03-03 14:55:51 -0800

    setup coveralls to fix the travis build

--- main.js
+++ main.js
@@ -5,6 +5,6 @@ const debug = require('debug')('HomeSecure:server');
 const server = require('./server');
 
 server.listen(process.env.PORT, () => {
-  debug('Running server');
+  debug('Running server cool');
   console.log('Server is up at port, ', process.env.PORT);
 });
