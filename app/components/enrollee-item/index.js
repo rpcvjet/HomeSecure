96332a5d43e4950cf14dbaac69edfc3a14dff5d2
Stephen Anderson <sa3196a@student.american.edu>
2017-03-07 17:20:41 -0800

    added enrollee-item component. debugged enrollee service

---
+++ app/components/enrollee-item/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+require('angular').module('homeSecure')
+.component('enrolleeItem', {
+  template: require('./enrollee-item.html'),
+  bindings: {
+    enrollee: '<',
+    handleSubmit: '<',
+  },
+});
